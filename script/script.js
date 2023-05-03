d84c36d84839de1467f9fa835acb9728b2ffaa1b
Vycheslav Smirnov <smirn.006vychislavoo@gmail.com>
2023-05-03 12:41:13 +0300

    Added ajax content loading.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,24 @@
 $(function() {
-    $('.nav__btn').on('click', function() {
+    // Button animation
+    $("[data-role='burger-btn']").on('click', function() {
         $('.wrapper__nav-list').toggleClass('wrapper__nav-list--active');
         $(this).toggleClass('nav__btn--active');
     });
+
+    // Navigation link
+    let infoContainer = $('#info');
+    $('[data-role="nav"]').on('click', (e) => {
+        e.preventDefault();
+        $('.wrapper__nav-list').toggleClass('wrapper__nav-list--active');
+        $("[data-role='burger-btn']").toggleClass('nav__btn--active');
+        $.post(e.target.href,
+            function (data) {
+                if (!data) {
+                    infoContainer.html(`<H3 class="system-information">Файл "${e.target.textContent}" находится в разработке`);
+                } else {
+                    infoContainer.html(data);
+                }
+            }
+        );
+    })
 })
